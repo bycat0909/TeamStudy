7107dc2633cf914f7c56b55bdf4bb55151fb1ca9
xit <xit@DESKTOP-8MVG1M3>
2021-02-02 15:00:18 +0900

    [2021.02.02] 백준알고리즘 팩토리얼 문제 완료

--- TeamStudy/src/jung/BOK10872.java
+++ TeamStudy/src/jung/BOK10872.java
@@ -1,23 +1,26 @@
 package jung;
 
+import java.io.IOException;
 import java.util.Scanner;
 
-/*
- * 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
- * 첫째 줄에 정수 N(0 ≤ N ≤ 12)가 주어진다.
- */
-public class Main {
+public class BOK10872 {
 
-	public static void main(String[] args) {
+	/*
+	 * 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+	 * 첫째 줄에 정수 N(0 ≤ N ≤ 12)가 주어진다.
+	 */
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		Scanner scan = new Scanner(System.in);
 		while(scan.hasNextInt()) {
 			int factorial = scan.nextInt();
 			if(factorial >= 0 && factorial <=12) {
 				int result= 1;
+				//while문 사용
 				while (factorial>0) {
 					result *= factorial;
 					factorial--;
 				}
+				//for문 사용
 //				for (; factorial > 0; factorial--) {
 //					result *= factorial;
 //				}
@@ -26,5 +29,4 @@ public class Main {
 		}
 		scan.close();							//스캐너 종료
 	}
-
 }
