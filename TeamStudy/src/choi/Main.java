30be71a11647252c754b868ef401b70624141c11
bycat <bycat0909@gmail.com>
2021-02-15 02:39:10 +0900

    반영

--- TeamStudy/src/choi/Main.java
+++ TeamStudy/src/choi/Main.java
@@ -1,44 +1,40 @@
 package choi;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-/*
-0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다. 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다. 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다. 다음 예를 보자.
-
-26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다. 6+8 = 14이다. 새로운 수는 84이다. 8+4 = 12이다. 새로운 수는 42이다. 4+2 = 6이다. 새로운 수는 26이다.
-위의 예는 4번만에 원래 수로 돌아올 수 있다. 따라서 26의 사이클의 길이는 4이다.
-N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오
-
-1.
-*/
 public class Main {
-	public static void main(String[] args) throws Exception{
 
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		int val = Integer.parseInt(br.readLine()); 	//초기값
-		int cnt = 0;								//사이클 카운팅
-		boolean ck = true;							//루프 끝낼 변수
-		int frs = 0;								//앞자리값 즉 sec
-		int sec = 0;								//뒷자리값 즉 sum의 뒷자리수
-		int sum = val;								//frs 와 sec의 합
-
-		while(ck){
-			cnt++;
-			frs = sum/10;
-			sec = sum%10;
-			sum = frs + sec;
-			if(sum<10){
-				sum = (sec*10) + sum;
-			}else{
-				sum = (sec*10) + (sum%10);
-			}
-			if(sum == val) ck = false;
-
+	/*
+	 * BOK 10872
+	 * 재귀함수 팩토리얼
+	 * 1 = 1
+	 * 2 = 2
+	 * 3 = 6
+	 * 4 = 24
+	 * 5 = 120
+	 * 6 = 720 
+	 * 7 = 5040
+	 * 8 = 40320
+	 * 9 = 362880
+	 * 10 = 3628800
+	 * 11 = 39916800
+	 * 12 = 479001600
+	 */
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(factorial(sc.nextInt()));
+	}
+	
+	public static int factorial(int a){
+		if(a <= 1){
+			return  a == 0 ? 1 : a;
+		}else{
+			return factorial(a - 1) * a;
 		}
-
-		System.out.println(cnt);
-
 	}
 }
