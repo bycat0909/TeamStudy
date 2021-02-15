0eac6a9e1b5a17c02071b781694a9f5c9a16fc1d
kimdonggyu <kimdonggyu@DESKTOP-B12IMSQ>
2021-02-15 10:31:53 +0900

    엑스아이티 김동규 사원 백준 알고리즘  스터디 10872번 반영

---
+++ TeamStudy/src/kdg/Study10872.java
@@ -0,0 +1,25 @@
+package kdg;
+
+import java.util.Scanner;
+//2021-02-08 XIT 스터디
+public class Study10872 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		scan.close();
+		
+		int sum = factorial(num);
+		System.out.println(sum);
+	}
+	
+	public static int factorial(int num) {
+		if(num <= 1) {
+			return 1;
+		}
+		return num * factorial(num - 1);		
+	}
+
+}
