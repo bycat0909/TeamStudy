d229925cf6303b4afe5143ab9649815d3c659f25
surinim <surinim@naver.com>
2021-02-20 22:17:18 +0900

    [2021.02.20] 시간초과 해결 - 이미 검사한 알파벳은 검사에서 제외   . 검사한 알파벳을 map에 담아 map에 존재하지 않는 알파벳만 사용 개수 셈..

--- TeamStudy/src/jung/BOK1157.java
+++ TeamStudy/src/jung/BOK1157.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-/*
- * 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 
- * 단, 대문자와 소문자를 구분하지 않는다.
- */
-public class Main {
+public class BOK1157 {
 
+	/*
+	 * 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 
+	 * 단, 대문자와 소문자를 구분하지 않는다.
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		char[] word = scan.next().toUpperCase().toCharArray();					//입력받은 단어의 문자배열
